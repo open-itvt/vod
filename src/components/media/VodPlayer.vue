ef21ae9a46900297321e01b9d180a7b675540644
klb-privacy <49614906+Klubuntu@users.noreply.github.com>
2026-06-20 19:14:25 +0200

    [Feat] Loading animacja w playerze Odysee (VodPlayer)

--- src/components/media/VodPlayer.vue
+++ src/components/media/VodPlayer.vue
@@ -1,20 +1,27 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 defineProps<{
   videoName: string
   claimId: string
 }>()
+
+const loaded = ref(false)
 </script>
 
 <template>
   <div class="embed-wrap">
+    <div v-if="!loaded" class="embed-loader" />
     <iframe
       width="100%"
       height="100%"
       :src="`https://odysee.com/$/embed/${videoName}/${claimId}`"
       class="odysee-iframe"
+      :class="{ loaded }"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
       allowfullscreen
       title="Media Embed"
+      @load="loaded = true"
     />
   </div>
 </template>
@@ -29,6 +36,38 @@ defineProps<{
   overflow: hidden;
 }
 
+.embed-loader {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 120px;
+  height: 24px;
+  background:
+    linear-gradient(#15bf7a 50%,#0000 0),
+    linear-gradient(#0000 50%,#15bf7a 0),
+    linear-gradient(#15bf7a 50%,#0000 0),
+    linear-gradient(#0000 50%,#15bf7a 0),
+    linear-gradient(#15bf7a 50%,#0000 0),
+    linear-gradient(#0000 50%,#15bf7a 0)
+    #333;
+  background-size: calc(100%/6 + 1px) 200%;
+  background-repeat: no-repeat;
+  border-radius: 4px;
+  animation: l12 1.5s infinite;
+  z-index: 1;
+}
+
+@keyframes l12 {
+  0%     { background-position: calc(0*100%/5) 100%,calc(1*100%/5)   0%,calc(2*100%/5) 100%,calc(3*100%/5)   0%,calc(4*100%/5) 100%,calc(5*100%/5)   0% }
+  16.67% { background-position: calc(0*100%/5)   0%,calc(1*100%/5)   0%,calc(2*100%/5) 100%,calc(3*100%/5)   0%,calc(4*100%/5) 100%,calc(5*100%/5)   0% }
+  33.33% { background-position: calc(0*100%/5)   0%,calc(1*100%/5) 100%,calc(2*100%/5) 100%,calc(3*100%/5)   0%,calc(4*100%/5) 100%,calc(5*100%/5)   0% }
+  50%    { background-position: calc(0*100%/5)   0%,calc(1*100%/5) 100%,calc(2*100%/5)   0%,calc(3*100%/5)   0%,calc(4*100%/5) 100%,calc(5*100%/5)   0% }
+  66.67% { background-position: calc(0*100%/5)   0%,calc(1*100%/5) 100%,calc(2*100%/5)   0%,calc(3*100%/5) 100%,calc(4*100%/5) 100%,calc(5*100%/5)   0% }
+  83.33% { background-position: calc(0*100%/5)   0%,calc(1*100%/5) 100%,calc(2*100%/5)   0%,calc(3*100%/5) 100%,calc(4*100%/5)   0%,calc(5*100%/5)   0% }
+  100%   { background-position: calc(0*100%/5)   0%,calc(1*100%/5) 100%,calc(2*100%/5)   0%,calc(3*100%/5) 100%,calc(4*100%/5)   0%,calc(5*100%/5) 100% }
+}
+
 .odysee-iframe {
   position: absolute;
   top: 0;
@@ -36,5 +75,12 @@ defineProps<{
   width: 100%;
   height: 100%;
   border: 0;
+  opacity: 0;
+  transition: opacity 0.4s ease;
+  z-index: 2;
+}
+
+.odysee-iframe.loaded {
+  opacity: 1;
 }
 </style>
